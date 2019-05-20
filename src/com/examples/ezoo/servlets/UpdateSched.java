4c91595c7ef12d8adfa0aa718560fe2af29fd4df
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-19 23:24:30 -0400

    fixed github indent corruption

--- src/com/examples/ezoo/servlets/UpdateSched.java
+++ src/com/examples/ezoo/servlets/UpdateSched.java
@@ -39,7 +39,7 @@ public class UpdateSched extends HttpServlet {
 		try {
 			
 			String recurrence = request.getParameter("recurrence");
-            String food= request.getParameter("food");
+            		String food= request.getParameter("food");
 			String notes = request.getParameter("notes");
 			int id = Integer.parseInt(request.getParameter("id"));
 			String time = request.getParameter("time");
@@ -50,47 +50,45 @@ public class UpdateSched extends HttpServlet {
 				
 				String sql = "UPDATE FEEDING_SCHEDULES SET RECURRENCE = (?) WHERE schedule_id = (?)";		
 				PreparedStatement stmt1 = c.prepareStatement(sql);
-                stmt1.setString(1,recurrence);
+                		stmt1.setString(1,recurrence);
 				stmt1.setInt(2,id);
 				stmt1.execute();
 				stmt1.close();
 			}
-           if (!food.equals("")){
+           		if (!food.equals("")){
         		
-        	   String sql = "UPDATE FEEDING_SCHEDULES SET food = (?) WHERE schedule_id = (?)";		
+        	   		String sql = "UPDATE FEEDING_SCHEDULES SET food = (?) WHERE schedule_id = (?)";		
 				PreparedStatement stmt2 = c.prepareStatement(sql);
-                stmt2.setString(1,food);
+                		stmt2.setString(1,food);
 				stmt2.setInt(2,id);
 				stmt2.execute();
 				 stmt2.close();
-           }
-           if (!notes.equals("")){
+           		}
+          		if (!notes.equals("")){
         	   
-        		String sql = "UPDATE FEEDING_SCHEDULES SET notes = (?) WHERE schedule_id = (?)";		
+        			String sql = "UPDATE FEEDING_SCHEDULES SET notes = (?) WHERE schedule_id = (?)";		
 				PreparedStatement stmt3 = c.prepareStatement(sql);
-                stmt3.setString(1,notes);
+                		stmt3.setString(1,notes);
 				stmt3.setInt(2,id);
 				stmt3.execute();
 				stmt3.close();
-           }
-           if(!time.equals("")){
+           		}
+           		if(!time.equals("")){
         	   
-        		String sql = "UPDATE FEEDING_SCHEDULES SET feeding_time = (?) WHERE schedule_id = (?)";		
+        			String sql = "UPDATE FEEDING_SCHEDULES SET feeding_time = (?) WHERE schedule_id = (?)";		
 				PreparedStatement stmt4 = c.prepareStatement(sql);
-                stmt4.setString(1,time);
+                		stmt4.setString(1,time);
 				stmt4.setInt(2,id);
 				stmt4.execute();
 				stmt4.close();
-           }
+           		}
                   
-           c.close();
+           		c.close();
            
 		}catch(Exception e){
 			e.printStackTrace();
 		
-					
-		
-	}
+		}
 		request.getRequestDispatcher("updatesched.jsp").forward(request, response);
 		
 	}
