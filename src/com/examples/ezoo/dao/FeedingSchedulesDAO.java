95d685e8f4de6ed0a68c178341401ad27a8f33d7
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-19 23:12:49 -0400

    Update FeedingSchedulesDAO.java

--- src/com/examples/ezoo/dao/FeedingSchedulesDAO.java
+++ src/com/examples/ezoo/dao/FeedingSchedulesDAO.java
@@ -8,12 +8,12 @@ import com.examples.ezoo.model.Schedule;
 
 public interface FeedingSchedulesDAO 
 {
-public   List<Schedule> getAllScheds();
-public   List<SchedulAssignment> getSchedAssignments();
-public  void saveSched2DB(Schedule schedule) throws Exception;
-public  void deleteSchedFromDB(int id);
-public  Schedule getSched(int animalid);
-public  void assignSched(int animalid,int scheduleid);
-public  void cancelSched(int anid,int schedid);
+        public   List<Schedule> getAllScheds();
+        public   List<SchedulAssignment> getSchedAssignments();
+        public  void saveSched2DB(Schedule schedule) throws Exception;
+        public  void deleteSchedFromDB(int id);
+        public  Schedule getSched(int animalid);
+        public  void assignSched(int animalid,int scheduleid);
+        public  void cancelSched(int anid,int schedid);
 
 }
