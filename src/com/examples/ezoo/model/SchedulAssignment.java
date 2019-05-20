0b9813a170849b0a739f4718708e32dfba2f140d
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-19 23:16:02 -0400

    fixed indentation

--- src/com/examples/ezoo/model/SchedulAssignment.java
+++ src/com/examples/ezoo/model/SchedulAssignment.java
@@ -1,55 +1,55 @@
 package com.examples.ezoo.model;
 
-public class SchedulAssignment {
-
-	     private int animalID;
-		 private int scheduleID;
+public class SchedulAssignment 
+{
+	private int animalID;
+	private int scheduleID;
 		 
 		   
-		 public SchedulAssignment(int anid,int schedid)
-		{
-		 animalID=anid;
-		 scheduleID=schedid;
-		}
+	public SchedulAssignment(int anid,int schedid)
+	{
+		animalID=anid;
+		scheduleID=schedid;
+	}
 
 		     
 		
-		 public SchedulAssignment() {
-			// TODO Auto-generated constructor stub
-		}
+	public SchedulAssignment() {
+		// TODO Auto-generated constructor stub
+	}
 
 
 
-		public int getAnimalID() {
-			return animalID;
-		}
+	public int getAnimalID() {
+		return animalID;
+	}
 
 
 
-		public void setAnimalID(int animalID) {
-			this.animalID = animalID;
-		}
+	public void setAnimalID(int animalID) {
+		this.animalID = animalID;
+	}
 
 
 
-		public int getScheduleID() {
-			return scheduleID;
-		}
+	public int getScheduleID() {
+		return scheduleID;
+	}
 
 
 
-		public void setScheduleID(int scheduleID) {
-			this.scheduleID = scheduleID;
-		}
+	public void setScheduleID(int scheduleID) {
+		this.scheduleID = scheduleID;
+	}
 
 
 
 		
-		public String toString()
-		{
-			return("AnimalID: "+animalID+" ScheduleID: "+scheduleID);
-		}
-		}
+	public String toString()
+	{
+		return("AnimalID: "+animalID+" ScheduleID: "+scheduleID);
+	}
+}
 
 			
 
